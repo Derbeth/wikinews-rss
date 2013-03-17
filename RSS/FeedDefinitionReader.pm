6fd885c83473cf0adb586264381e06c528ccbf19
Derbeth <derbeth@interia.pl>
2013-03-17 14:31:15 +0100

    sources.yml: introduced domain to reduce copy and paste for lang, copyright and domain address

--- RSS/FeedDefinitionReader.pm
+++ RSS/FeedDefinitionReader.pm
@@ -24,29 +24,31 @@ sub read {
 	open(my $fh, "<:encoding(UTF-8)", $self->{'source'}) || die "cannot read $self->{source}: $!";
 	my $all = LoadFile($self->{'source'});
 	close($fh);
+	my $domains = $all->{domains};
 	my @defs;
 	foreach my $doc (@{$all->{sources}}) {
+		my $domain = $domains->{$doc->{domain}} || die "invalid domain: $doc->{domain}";
 		my $feed = new RSS::Feed($doc->{output_file},
 			$doc->{title},
 			$doc->{page_url},
 			$doc->{description},
-			$doc->{language},
+			$domain->{language},
 			$doc->{feed_link});
 		$feed->setImage($doc->{logo}->{url},
 			$doc->{title},
 			$doc->{page_url},
 			$doc->{logo}->{width},
 			$doc->{logo}->{height});
-		$feed->setCopyright($doc->{copyright});
+		$feed->setCopyright($domain->{copyright});
 
 		my $check_interval = $doc->{check_interval};
 		$check_interval = $RSS::Settings::FORCED_FEED_CHECK_INTERVAL if $RSS::Settings::FORCED_FEED_CHECK_INTERVAL;
-		my $link_prefix = $doc->{link_prefix} || "http://$doc->{domain}";
+		my $link_prefix = $domain->{link_prefix} || "http://$domain->{domain}";
 		my $max_new_news = $doc->{max_new_news} || $RSS::Settings::DEFAULT_MAX_NEW_NEWS;
 
 		my $news_source = new RSS::NewsSource($check_interval,
 			$link_prefix,
-			$doc->{domain},
+			$domain->{domain},
 			$doc->{source},
 			$doc->{source_type},
 			$max_new_news);
